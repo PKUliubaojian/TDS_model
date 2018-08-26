dfa5c986156416d964fdd07082e203ba5ccc302a
刘宝剑 <1327157006@qq.com>
2018-08-26 12:36:14 +0800

    geopandas and shapely example

---
+++ Python/shp_read_example.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 00:27:59 2018
+
+@author: lenovo
+"""
+import shapefile
+import shapely
+import geopandas as gpd
+from shapely.geometry import POINT
+path_land="G:\\data\\ne_10m_land\\ne_10m_land.shp"
+path_lake="G:\\data\\ne_10m_lakes\\ne_10m_lakes.shp"
+sr=shapefile.Reader(path_lake)
+shapes=sr.shapes()
+
+s_land=gpd.read_file(path_land)
+s_lake=gpd.read_file(path_lake)
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+world.plot()
+pt=[Point(2,2),Point(2,3),Point(3,3)]
+pts=gpd.GeoDataFrame(geometry=pt)
+polygon1=geometry.Polygon([(2.5,1),(1,3),(3,3),(3,1)])
+polygon2=geometry.Polygon([(-1,1),(1,3),(3,3),(3,1)])
+pys=gpd.GeoDataFrame(geometry=[polygon1,polygon2])
+union=pts["geometry"].unary_union
+pts.geometry.within(pys.geometry)

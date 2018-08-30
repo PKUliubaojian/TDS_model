6437414eb2316cbb284b75bdd98c5e09336c1a21
刘宝剑 <1327157006@qq.com>
2018-08-30 13:22:47 +0800

    Change input of Point in Polygon

--- Python/Points_in_Polygon.py
+++ Python/Points_in_Polygon.py
@@ -7,21 +7,23 @@ Created on Sun Aug 26 00:27:59 2018
 
 import geopandas as gpd
 from shapely.geometry import Point,Polygon
-import pandas
+import numpy as np
 from pandas import DataFrame,Series
 
 
-def pixelswithin(points,source="land"):
+def pixelswithin(Lon,Lat,source="land"):
     '''Points in shapeflie'''
-    if source == 'land':
+    if source in ['land']:
         path="shp\\ne_110m_land\\ne_110m_land.shp"
 
     elif source=='lake':
         path="shp\\ne_10m_lakes\\ne_10m_lakes.shp"
     else:
         path=source
+    df=DataFrame({"Coordinates":list(zip(Lon,Lat))})
+    df['Coordinates'] = df['Coordinates'].apply(Point)
+    pts = gpd.GeoDataFrame(df, geometry='Coordinates')
     shapes=gpd.read_file(path)
-    pts=gpd.GeoDataFrame(geometry=points)
     union=shapes["geometry"].unary_union
     pts_in=pts.geometry.within(union)
     return pts_in

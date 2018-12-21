e2f3646b1badfa77c5cee354b9aaa8bb47732721
Baojian <1327157006@qq.com>
2018-12-21 21:04:20 +0800

    移动了两个文件的位置

--- Python/CYGNSS.py
+++ Python/CYGNSS.py
@@ -24,8 +24,17 @@ class DDM_L1:
         Alt=np.array(DDM['sp_alt']).reshape(-1)
         LES=np.array(DDM['ddm_les']).reshape(-1)
         nbrcs=np.array(DDM['ddm_nbrcs']).reshape(-1)
+        P_t=np.array(DDM['gps_tx_power_db_w']).reshape(-1)
+        G_t=np.array(DDM['gps_ant_gain_db_i']).reshape(-1)
+        G_r=np.array(DDM['sp_rx_gain']).reshape(-1)
+        R_t=np.array(DDM['tx_to_sp_range']).reshape(-1)
+        R_r=np.array(DDM['rx_to_sp_range']).reshape(-1)
+        I_angle=np.array(DDM['sp_inc_angle']).reshape(-1)
         SNR=np.array(DDM['ddm_snr'][:]).reshape(-1)
-        df=DataFrame({'ts':ts,'Lat':Lat,'Lon':Lon,'Alt':Alt,'SNR':SNR,'nbrcs':nbrcs,'LES':LES})
+        
+        df=DataFrame({'ts':ts,'Lat':Lat,'Lon':Lon,'Alt':Alt,'SNR':SNR,'nbrcs':nbrcs,
+                      'LES':LES,'P_t':P_t,'G_t':G_t,'G_r':G_r,'R_t':R_t,'R_r':R_r,
+                      'I_angle':I_angle})
         mask=(df["LES"]!=-9999)&(df["nbrcs"]!=-9999)&(df["SNR"]!=-9999)
         ddms=np.array(DDM['brcs'][:]).reshape(-1,17,11)
         self.ddms=ddms[mask,:,:]

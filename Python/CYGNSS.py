a96e6f227b8daeb7aea8a5a781befae8e3dfb0ab
Baojian <1327157006@qq.com>
2018-12-28 23:00:04 +0800

    合并了python2的两个文件

--- Python/CYGNSS.py
+++ Python/CYGNSS.py
@@ -29,13 +29,99 @@ class DDM_L1:
         G_r=np.array(DDM['sp_rx_gain']).reshape(-1)
         R_t=np.array(DDM['tx_to_sp_range']).reshape(-1)
         R_r=np.array(DDM['rx_to_sp_range']).reshape(-1)
+        Track_ID=np.array(DDM['track_id']).reshape(-1)
         I_angle=np.array(DDM['sp_inc_angle']).reshape(-1)
         SNR=np.array(DDM['ddm_snr'][:]).reshape(-1)
-        
+        nbrcs_area=np.array(DDM['nbrcs_scatter_area'][:]).reshape(-1)
+        peak_row=np.array(DDM['brcs_ddm_peak_bin_delay_row'][:]).reshape(-1)
+        peak_col=np.array(DDM['brcs_ddm_peak_bin_dopp_col'][:]).reshape(-1)
         df=DataFrame({'ts':ts,'Lat':Lat,'Lon':Lon,'Alt':Alt,'SNR':SNR,'nbrcs':nbrcs,
-                      'LES':LES,'P_t':P_t,'G_t':G_t,'G_r':G_r,'R_t':R_t,'R_r':R_r,
-                      'I_angle':I_angle})
+              'LES':LES,'P_t':P_t,'G_t':G_t,'G_r':G_r,'R_t':R_t,'R_r':R_r,
+              'I_angle':I_angle,'Track_ID':Track_ID,'nbrcs_area':nbrcs_area,
+              'peak_row':peak_row,'peak_col':peak_col})
         mask=(df["LES"]!=-9999)&(df["nbrcs"]!=-9999)&(df["SNR"]!=-9999)
         ddms=np.array(DDM['brcs'][:]).reshape(-1,17,11)
+        areas=np.array(DDM['eff_scatter'][:]).reshape(-1,17,11)
         self.ddms=ddms[mask,:,:]
         self.df=df[mask]
+        self.areas=areas[mask]
+        
+def DDMA_CY(ddms,areas,p_delay,p_doppler,delaybin=3,dopplerbin=5):
+    """
+        输入的ddms和area都是一个三维矩阵，第一个维度为ddm数量，第二个维度delay固定为17，第三个维度doppler固定为10
+        ddms: CYGNSS L1b DDM
+        area: CYGNSS L1b effective scattering area
+        p_delay: CYGNSS L1b peak delay bin in pixel
+        p_doppler: CYGNSS L1b peak doppler bin in pixel
+        输出:DDMA
+        建议把ddm串起来计算以提高计算效率
+    """
+    N=np.arange(ddms.shape[0])
+    x_d=delaybin//2
+    y_d=dopplerbin//2
+    x_l,y_l=np.meshgrid(np.arange(-x_d,x_d+1,1),np.arange(-y_d,y_d+1,1))
+    x_l=x_l.reshape(-1)
+    y_l=y_l.reshape(-1)
+    sum_brcs=[ddms[N,(p_delay+dx)%16,(p_doppler+dy)%10] for (dx,dy) in zip(x_l,y_l)]
+    sum_area=[areas[N,(p_delay+dx)%16,(p_doppler+dy)%10] for (dx,dy) in zip(x_l,y_l)]
+    c_brcs=np.stack(sum_brcs)
+    c_area=np.stack(sum_area)
+    DDMA=np.sum(c_brcs,axis=0)/np.sum(c_area,axis=0)
+    return DDMA
+
+def LES_CY(ddms,areas,p_delay,p_doppler,delaybin=3,dopplerbin=5):
+    """
+        输入的ddms和area都是一个三维矩阵，第一个维度为ddm数量，第二个维度delay固定为17，第三个维度doppler固定为10
+        ddms: CYGNSS L1b DDM
+        area: CYGNSS L1b effective scattering area
+        p_delay: CYGNSS L1b peak delay bin in pixel
+        p_doppler: CYGNSS L1b peak doppler bin in pixel
+        输出：LES
+        建议把ddm串起来计算以提高计算效率
+    """
+    N=np.arange(ddms.shape[0])
+    x_d=delaybin//2
+    y_d=dopplerbin//2
+    x_series=np.arange(-x_d,x_d+1,1)
+    y_series=np.arange(-y_d,y_d+1,1)
+    x_l,y_l=np.meshgrid(x_series,y_series)
+    x_l=x_l.reshape(-1)
+    y_l=y_l.reshape(-1)
+    sum_area=[areas[N,(p_delay+dx)%16,(p_doppler+dy)%10] for (dx,dy) in zip(x_l,y_l)]
+    c_area=np.sum(np.stack(sum_area),axis=0)    
+    peak_IDW=[ddms[N,p_delay,(p_doppler+dy)%10] for dy in y_series]
+    leading_IDW=[ddms[N,(p_delay-delaybin),(p_doppler+dy)%10] for dy in y_series]
+    peak_IDW=np.sum(np.stack(peak_IDW),axis=0)
+    leading_IDW=np.sum(np.stack(leading_IDW),axis=0)
+    leading=(peak_IDW-leading_IDW)/delaybin
+    LES=leading/c_area
+    return LES
+
+def TES_CY(ddms,areas,p_delay,p_doppler,delaybin=5,dopplerbin=5):
+    """
+        输入的ddms和area都是一个三维矩阵，第一个维度为ddm数量，第二个维度delay固定为17，第三个维度doppler固定为10
+        ddms: CYGNSS L1b DDM
+        area: CYGNSS L1b effective scattering area
+        p_delay: CYGNSS L1b peak delay bin in pixel
+        p_doppler: CYGNSS L1b peak doppler bin in pixel
+        输出：TES
+        建议把ddm串起来计算以提高计算效率
+    """
+    N=np.arange(ddms.shape[0])
+    x_d=delaybin//2
+    y_d=dopplerbin//2
+    x_series=np.arange(-x_d,x_d+1,1)
+    y_series=np.arange(-y_d,y_d+1,1)
+    x_l,y_l=np.meshgrid(x_series,y_series)
+    x_l=x_l.reshape(-1)
+    y_l=y_l.reshape(-1)
+    sum_area=[areas[N,(p_delay+dx)%16,(p_doppler+dy)%10] for (dx,dy) in zip(x_l,y_l)]
+    peak_IDW=[ddms[N,p_delay,(p_doppler+dy)%10] for dy in y_series]
+    trailing_IDW=[ddms[N,(p_delay+delaybin)%17,(p_doppler+dy)%10] for dy in y_series]
+    c_area=np.sum(np.stack(sum_area),axis=0)
+    peak_IDW=np.sum(np.stack(peak_IDW),axis=0)
+    trailing_IDW=np.sum(np.stack(trailing_IDW),axis=0)
+    trailing=(peak_IDW-trailing_IDW)/delaybin
+    TES=trailing/c_area
+    return TES
+
--- Python/csv2shp.py
+++ Python/csv2shp.py
@@ -4,40 +4,58 @@
 # import system modules 
 import arcpy
 import os
+import arcgisscripting, sys, string
+def csv2shp(workspace,outfolder):
 # Set environment settings
 #这里分别是输入和输出文件夹
-arcpy.env.workspace = "D:\data\GNSS\CYGNSS"
-output="D:\data\GNSS\CYGNSS2"
-ws=arcpy.env.workspace
-try:
-    files=os.listdir(ws)
-    #没有做额外的筛选，只支持.csv文件，不太建议文件夹下有别的文件类型
-    for name in files:
-    # Set the local variables
-        in_Table = name
-        #经纬度字段要和csv保持一致
-        x_coords = "Lon"
-        y_coords = "Lat"
-        temp=name.strip(".csv")
-        out_Layer = temp+"layer"
-        saved_Layer = temp+".shp"
-        # Set the spatial reference
-        spRef = r"Coordinate Systems\Geographic Coordinate Systems\World\WGS 1984.prj"
-        # Make the XY event layer...
-        arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer,spRef)
-        # Print the total rows
-        print(arcpy.GetCount_management(out_Layer))
+    arcpy.env.workspace = workspace
+    output=outfolder
+    ws=workspace
+    try:
+        files=os.listdir(ws)
+        #没有做额外的筛选，只支持.csv文件，不太建议文件夹下有别的文件类型
+        for name in files:
+        # Set the local variables
+            in_Table = name
+            #经纬度字段要和csv保持一致
+            x_coords = "Lon"
+            y_coords = "Lat"
+            temp=name.strip(".csv")
+            out_Layer = temp+"layer"
+            saved_Layer = temp+".shp"
+            # Set the spatial reference
+            spRef = r"Coordinate Systems\Geographic Coordinate Systems\World\WGS 1984.prj"
+            # Make the XY event layer...
+            arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer,spRef)
+            # Print the total rows
+            print(arcpy.GetCount_management(out_Layer))
+            
+            # Save to a layer file
+            arcpy.FeatureClassToFeatureClass_conversion(out_Layer,output, saved_Layer)
+            
+            Input_Table = output+'\\'+saved_Layer
+            # Process: Add Field
+            arcpy.AddField_management(Input_Table, "SNR2", "DOUBLE", "", "", "", "", "NON_NULLABLE", "NON_REQUIRED", "")
+            # Process: Calculate Field
+            arcpy.CalculateField_management(Input_Table, "SNR2", "CDbl([SNR])", "VB", "")
+    except Exception as err:
+        print(err.args[0])
         
-        # Save to a layer file
-        arcpy.FeatureClassToFeatureClass_conversion(out_Layer,output, saved_Layer)
-        
-        Input_Table = output+'\\'+saved_Layer
-        # Process: Add Field
-        arcpy.AddField_management(Input_Table, "SNR2", "DOUBLE", "", "", "", "", "NON_NULLABLE", "NON_REQUIRED", "")
-        # Process: Calculate Field
-        arcpy.CalculateField_management(Input_Table, "SNR2", "CDbl([SNR])", "VB", "")
-
-
-except Exception as err:
-    print(err.args[0])
+def txt2shp(pathin,pathout):
+    gp = arcgisscripting.create()
+    try:
+        files=os.listdir(pathin)
+        for name in files:
+            in_Table = pathin+"/"+name
+            in_x = "x"
+            in_y = "y"
+            temp=name.strip(".csv")
+            out_Layer =temp.replace("-","_")
+            spref = r"Coordinate Systems\Geographic Coordinate Systems\World\WGS 1984.prj"
+            gp.MakeXYEventLayer(in_Table, in_x, in_y, out_Layer, spref)
+            pDSC = gp.describe(out_Layer)
+            print gp.getcount(out_Layer)
+            gp.FeatureClassToShapefile(out_Layer,pathout)
+    except:
+        print gp.GetMessages()
 
